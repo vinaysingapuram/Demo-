30cb506ea29c339c3ae95debd92111cb2c59127b
vinaysingapuram <svinaykumar241@yahoo.co.uk>
2019-11-27 23:19:55 +0200

    First GIT commit for my Selenium Code

---
+++ src/test/java/co/qa/tests/DemoTest.java
@@ -0,0 +1,38 @@
+package co.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	
+	@Test	
+	public void sum() {
+		System.out.println("Sum");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30,a+b);
+	}
+	@Test
+	public void sub() {
+		System.out.println("Sub");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10,b-a);
+	}
+	@Test
+	public void div	() {
+		System.out.println("Div");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2,b/a);
+	}
+	@Test
+	public void mul() {
+		System.out.println("Sum");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200,a*b);
+	}
+	
+}
